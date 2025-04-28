f1764b6aa3bf5d10e44ba9f711de40cd0bca1d73
kyasbal <ikakeru@google.com>
2025-04-28 11:14:21 +0900

    Added reader and serializer
    * Adds NodeReader type to read fields from the Node interface with field paths
    * Adds serializing related features on Node implementations and NodeReader

--- pkg/common/structurev2/standard.go
+++ pkg/common/structurev2/standard.go
@@ -14,8 +14,18 @@
 
 package structurev2
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
 // standard.go contains types to hold structured data on memory as its field and implements Node interface.
 
+// StandardScalarNode is a leaf of structured data implemting Node interface.
 type StandardScalarNode[T any] struct {
 	value T
 }
@@ -32,6 +42,61 @@ func (n *StandardScalarNode[T]) Children() NodeChildrenIterator {
 	return func(func(key NodeChildrenKey, value Node) bool) {}
 }
 
+// MarshalJSON implements json.Marshaler.
+func (n *StandardScalarNode[T]) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	value, err := json.Marshal(n.value)
+	if err != nil {
+		return nil, err
+	}
+	_, err = buf.Write(value)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// MarshalYAML implements yaml.Marshaler.
+func (n *StandardScalarNode[T]) MarshalYAML() (interface{}, error) {
+	yamlNode := &yaml.Node{
+		Kind: yaml.ScalarNode,
+	}
+	anyValue, err := n.NodeScalarValue()
+	if err != nil {
+		return nil, err
+	}
+	if anyValue == nil {
+		yamlNode.Tag = "!!null"
+		yamlNode.Value = "null"
+	} else {
+		switch value := anyValue.(type) {
+		case string:
+			yamlNode.Tag = "!!str"
+			yamlNode.Value = value
+		case int:
+			yamlNode.Tag = "!!int"
+			yamlNode.Value = fmt.Sprintf("%d", value)
+		case bool:
+			yamlNode.Tag = "!!bool"
+			yamlNode.Value = fmt.Sprintf("%t", value)
+		case float64:
+			yamlNode.Tag = "!!float"
+			yamlNode.Value = fmt.Sprintf("%f", value)
+		case time.Time:
+			yamlNode.Tag = "!!timestamp"
+			yamlNode.Value = value.Format(time.RFC3339)
+		default:
+			return nil, fmt.Errorf("unsupported scalar type: %T", value)
+		}
+	}
+	return yamlNode, nil
+}
+
+var _ Node = (*StandardScalarNode[any])(nil)
+var _ json.Marshaler = (*StandardScalarNode[any])(nil)
+var _ yaml.Marshaler = (*StandardScalarNode[any])(nil)
+
+// StandardScalarNode is a sequence field of a structured data implementing Node interface.
 type StandardSequenceNode struct {
 	value []Node
 }
@@ -54,6 +119,60 @@ func (n *StandardSequenceNode) Children() NodeChildrenIterator {
 	}
 }
 
+// MarshalYAML implements yaml.Marshaler.
+func (n *StandardSequenceNode) MarshalYAML() (interface{}, error) {
+	sequenceNode := &yaml.Node{
+		Kind:    yaml.SequenceNode,
+		Tag:     "!!seq",
+		Content: []*yaml.Node{},
+	}
+	for _, child := range n.Children() {
+		marshaller, ok := child.(yaml.Marshaler)
+		if !ok {
+			return nil, fmt.Errorf("sequence node child is not implementing yaml.Marshaller")
+		}
+		node, err := marshaller.MarshalYAML()
+		if err != nil {
+			return nil, err
+		}
+		yamlNode, ok := node.(*yaml.Node)
+		if !ok {
+			return nil, fmt.Errorf("sequence node child returned non *yaml.Node as its result")
+		}
+		sequenceNode.Content = append(sequenceNode.Content, yamlNode)
+	}
+	return sequenceNode, nil
+}
+
+// MarshalJSON implements json.Marshaler.
+func (n *StandardSequenceNode) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteString("[")
+	for i, child := range n.Children() {
+		if i.Index > 0 {
+			buf.WriteString(",")
+		}
+		marshaller, ok := child.(json.Marshaler)
+		if !ok {
+			return nil, fmt.Errorf("sequence node child is not implementing json.Marshaller")
+		}
+		marshalled, err := marshaller.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(marshalled)
+	}
+	buf.WriteString("]")
+
+	return buf.Bytes(), nil
+}
+
+var _ Node = (*StandardSequenceNode)(nil)
+var _ yaml.Marshaler = (*StandardSequenceNode)(nil)
+var _ json.Marshaler = (*StandardSequenceNode)(nil)
+
+// StandardMapNode is a map field of structured data implementing Node interface.
+// This type retain the order of keys.
 type StandardMapNode struct {
 	// keys is the list of keys in values.
 	// Needed because the key order of map is not assured in Go.
@@ -76,5 +195,129 @@ func (n *StandardMapNode) Children() NodeChildrenIterator {
 				return
 			}
 		}
+	}
+}
+
+// MarshalYAML implements yaml.Marshaler.
+func (n *StandardMapNode) MarshalYAML() (interface{}, error) {
+	mapNode := &yaml.Node{
+		Kind:    yaml.MappingNode,
+		Tag:     "!!map",
+		Content: []*yaml.Node{},
+	}
+	for k, child := range n.Children() {
+		key := k.Key
+		marshaller, ok := child.(yaml.Marshaler)
+		if !ok {
+			return nil, fmt.Errorf("map node child is not implementing yaml.Marshaller")
+		}
+		node, err := marshaller.MarshalYAML()
+		if err != nil {
+			return nil, err
+		}
+		yamlNode, ok := node.(*yaml.Node)
+		if !ok {
+			return nil, fmt.Errorf("map node child returned non *yaml.Node as its result")
+		}
+		keyNode := &yaml.Node{
+			Kind:  yaml.ScalarNode,
+			Value: key,
+			Tag:   "!!str",
+		}
+		mapNode.Content = append(mapNode.Content, keyNode)
+		mapNode.Content = append(mapNode.Content, yamlNode)
+	}
+	return mapNode, nil
+}
+
+// MarshalJSON implements json.Marshaler.
+func (n *StandardMapNode) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.WriteString("{")
+	for i, child := range n.Children() {
+		if i.Index > 0 {
+			buf.WriteString(",")
+		}
+		key := fmt.Sprintf("\"%s\"", i.Key)
+		buf.WriteString(key)
+		buf.WriteString(":")
+		marshaller, ok := child.(json.Marshaler)
+		if !ok {
+			return nil, fmt.Errorf("map node child is not implementing json.Marshaller")
+		}
+		marshalled, err := marshaller.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(marshalled)
+	}
+	buf.WriteString("}")
+
+	return buf.Bytes(), nil
+
+}
+
+var _ Node = (*StandardMapNode)(nil)
+var _ yaml.Marshaler = (*StandardMapNode)(nil)
+var _ json.Marshaler = (*StandardMapNode)(nil)
+
+// getYAMLMarshaler returns the yaml.Marshaller from Node interface.
+func getYAMLMarshaler(node Node) (yaml.Marshaler, error) {
+	standardRootNode, err := cloneStandardNodeFromNode(node)
+	if err != nil {
+		return nil, err
+	}
+	return standardRootNode.(yaml.Marshaler), nil
+}
+
+// getJSONMarshalerFromNode returns the json.Marshaller from Node interface.
+func getJSONMarshalerFromNode(node Node) (json.Marshaler, error) {
+	standardRootNode, err := cloneStandardNodeFromNode(node)
+	if err != nil {
+		return nil, err
+	}
+	return standardRootNode.(json.Marshaler), nil
+}
+
+// cloneStandardNodeFromNode clones Node interface into Standard***Node.
+// Standard**Node implements json.Marshaller and yaml.Marshaller, it allows serializing any implementing Node interface.
+func cloneStandardNodeFromNode(node Node) (Node, error) {
+	switch node.Type() {
+	case ScalarNodeType:
+		scalarValue, err := node.NodeScalarValue()
+		if err != nil {
+			return nil, err
+		}
+		return &StandardScalarNode[any]{
+			value: scalarValue,
+		}, nil
+	case SequenceNodeType:
+		sequence := StandardSequenceNode{
+			value: make([]Node, 0),
+		}
+		for _, child := range node.Children() {
+			child, err := cloneStandardNodeFromNode(child)
+			if err != nil {
+				return nil, err
+			}
+			sequence.value = append(sequence.value, child)
+		}
+		return &sequence, nil
+	case MapNodeType:
+		mapNode := StandardMapNode{
+			keys:   make([]string, 0),
+			values: make([]Node, 0),
+		}
+		for key, child := range node.Children() {
+			mapNode.keys = append(mapNode.keys, key.Key)
+			child, err := cloneStandardNodeFromNode(child)
+			if err != nil {
+				return nil, err
+			}
+			mapNode.values = append(mapNode.values, child)
+		}
+		return &mapNode, nil
+	default:
+		return nil, fmt.Errorf("unknown node type: %v", node.Type())
 	}
 }
