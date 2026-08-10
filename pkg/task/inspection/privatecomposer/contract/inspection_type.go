c02f0fca58beb29bc52ba986c696cdc57a11e95f
ikakeru <ikakeru@google.com>
2026-08-10 17:06:06 +0900

    Fixed product name of Managed Airflow
    Change-Id: Ifb73e5620d91dd36c871bac86cefff566a6a6964

--- pkg/task/inspection/privatecomposer/contract/inspection_type.go
+++ pkg/task/inspection/privatecomposer/contract/inspection_type.go
@@ -28,8 +28,8 @@ var InspectionTypeId = "gcp-composer-v3"
 
 var ComposerV3InspectionType = coreinspection.InspectionType{
 	Id:          InspectionTypeId,
-	Name:        "Google Cloud Managed Airflow 3 (Internal)",
-	Description: "Fetch both Managed Airflow 3 (formerly Cloud Composer 3) backend and GKE tenant project logs to generate Managed Airflow 3 specific results.",
+	Name:        "Managed Service for Apache Airflow 3 (Internal)",
+	Description: "Fetch both Managed Service for Apache Airflow 3 backend and GKE tenant project logs to generate Managed Service for Apache Airflow 3 specific results.",
 	Icon:        "assets/icons/composer.webp",
 	Priority:    math.MaxInt - 5,
 	Labels: map[string]string{
